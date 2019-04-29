fe4e8b9391c5685e9cce9e260dbe1f5fad9b3194
bilal <mbilalghz@gmail.com>
2019-04-29 22:11:24 +0700

    edit config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require('./routes/index')
-const port = 3000
+const port = process.env.PORT || 4000
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use('/',routes)
